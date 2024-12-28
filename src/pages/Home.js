4b54355eda6140e18deee87bf15f3f8bb482513e
GhanshayamSharma <ishubhamsaini2003@gmail.com>
2024-12-28 18:05:34 +0530

    Updated styles and animations in App.css and global.scss, added new global.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,13 @@ function Home({ setCurrentPage }) {
         Explore my work, learn about my background, and see how I can help
         bring your ideas to life!
       </p>
+      <p>
+        As a 4th-year Computer Science Engineering student specializing in backend development, I possess a strong 
+        foundation in programming languages, data structures, and software engineering principles. With a keen interest in 
+        building scalable and efficient software systems, I aim to secure a backend development role that leverages my skills 
+        and provides opportunities for growth and learning. I am excited to apply my knowledge and expertise to contribute to 
+        innovative projects and collaborate with cross-functional teams to deliver high-quality solutions.
+      </p>
       <button className="btn" onClick={() => setCurrentPage("Projects")}>
         View My Projects
       </button>
